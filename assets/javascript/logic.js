4547603790a5ed31f8e9e2f707f73fdfde8c8719
Alex <43651369+tunkytime@users.noreply.github.com>
2019-03-22 16:25:01 -0600

    update html to include next train time

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -57,7 +57,6 @@ $("#add-train-btn").on("click", function (event) {
 
 // Firebase event to add train to the database and html
 database.ref().on("child_added", function (childSnapshot) {
-	console.log(childSnapshot.val());
 	
 	// Store into variables
 	var train = childSnapshot.val().train;
@@ -65,21 +64,57 @@ database.ref().on("child_added", function (childSnapshot) {
 	var time = childSnapshot.val().time;
 	var frequency = childSnapshot.val().frequency;
 	
-	// Train info
-	console.log(train);
-	console.log(destination);
-	console.log(time);
-	console.log(frequency);
+	// Convert variables using moment
+	var firstArrival = formatTime(time);
+	var currentTime = moment().format("hh:mm A");
+	var frequencyTime = frequency;
+	var schedule = [];
+	var remainingTimes = [];
+	var nextTrain;
+	var nextTrainFormatted;
+	
+	// Calculate next 20 arrival times
+	for (var i = 1; i < 21; i++) {
+		var calcArrivalTime = moment(firstArrival, "hh:mm A").add(frequencyTime * i, "minute");
+		arrivalTime = calcArrivalTime.format("hh:mm A");
+		schedule.push(arrivalTime);
+	};
+
+	for (var i = 0; i < schedule.length; i++) {
+		var a = moment(schedule[i], "hh:mm A");
+		var b = moment(currentTime, "hh:mm A");
+		var result = a.diff(b);
+		
+		if (result <= 3600000 && result > 0) {
+			remainingTimes.push(result);
+			nextTrain = Math.min(...remainingTimes);
+			nextTrainFormatted = moment.utc(nextTrain).format("00:mm");
+			console.log(nextTrainFormatted);
+			return false;
+		} 
+		if (result > 3600000) {
+			remainingTimes.push(result);
+			var nextTrain = Math.min(...remainingTimes);
+			var nextTrainFormatted = moment.utc(nextTrain).format("hh:mm");
+			console.log(nextTrainFormatted);
+			return false;
+		};
+	};
+		
+	console.log(`First Arrival: ${firstArrival}`);
+	console.log(`Current Time: ${currentTime}`); 
+	console.log(`Every ${frequencyTime} minutes`);
 	
 	// Create new row
 	var newRow = $("<tr>").append(
 		$("<td>").text(train),
 		$("<td>").text(destination),
-		$("<td>").text(time),
-		$("<td>").text(frequency),
+		$("<td>").text(firstArrival),
+		$("<td>").text(frequencyTime),
 	);
 	
 	// Append new row to table
 	$("#train-table > tbody").append(newRow);
 });
+
 
